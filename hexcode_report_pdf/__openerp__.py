c55071c61c7966cbd02dbd2d390fd59ad543d6fe
Federico Ranieri <federico.ranieri96@gmail.com>
2016-05-03 14:36:45 +0200

    Update __openerp__.py

--- hexcode_report_pdf/__openerp__.py
+++ hexcode_report_pdf/__openerp__.py
@@ -2,7 +2,8 @@
     'name': 'Report PDF',
     'version': '1.0',
     'category': 'app',
-    'description': '',
+    'description': 'HexCode pdf Report is a module that allows the creation of professional reports and can download them in PDF format. It currently supports pdf report for: Claims, Invoice, Sales Order, Quotations, Picking, Request for Quotations and Purchase Orders.',
+    'summary': 'Module for creating professional PDF reports',
     'author': 'Federico Ranieri',
     'website': 'www.federicoranieri.it',
     'depends': [
